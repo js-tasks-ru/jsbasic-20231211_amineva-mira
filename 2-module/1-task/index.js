ee51f525e71775e9ba1c467ac631ea710beb7406
thonngo <amineva.mira@yandex.ru>
2023-12-20 18:19:51 +0300

    done 1 task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum=0;
+  for (let key in salaries) {
+    // if((salaries[key] ^ 0)===salaries[key] && isFinite(salaries[key])) {
+    if((typeof salaries[key]) === 'number' && isFinite(salaries[key])) {
+      sum+=salaries[key];
+    }
+  }
+  return sum;
 }
